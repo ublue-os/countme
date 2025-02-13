d5ec686fe873d609d09613e4cc4ac024abfa66e0
Kyle Gospodnetich <me@kylegospodneti.ch>
2025-02-13 11:06:52 -0800

    Adjust size and aspect ratio

--- countme.py
+++ countme.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
+from dateutil.relativedelta import relativedelta
 
 plt.style.use("default")
 plt.style.use("./ublue.mplstyle")
@@ -55,8 +56,8 @@ d = d[
     # & (d["week_end"] != pd.to_datetime("2023-10-23"))
 ]
 
-START_DATE = "2024-07-01"  # "2021-3-1"
-END_DATE = datetime.datetime.now()  # "2025-02-02"
+START_DATE = datetime.datetime.now() - relativedelta(months=3)
+END_DATE = datetime.datetime.now()
 
 for fig, oss in [
     ("ublue", ["Bluefin", "Bazzite", "Aurora"]),
@@ -64,21 +65,24 @@ for fig, oss in [
     ("bazzite", ["Bazzite"]),
     ("global", ["Silverblue", "Kinoite", "Bluefin", "Bazzite", "Aurora"]),
 ]:
-    plt.figure()
+    
+    plt.figure(figsize=(16, 9))
     for os in oss:
         mask = d["os_variant"].str.lower().str.contains(os.lower(), na=False)
         res = d[mask].groupby("week_end")["hits"].sum()
         plt.plot(res.index, res.values, label=f"{os} ({res[res.index.max()] / 1000:.1f}k)")  # type: ignore
         # print(res)
 
-    plt.title("Active Devices per Week")
-    plt.xlabel("Week")
-    plt.ylabel("Hits")
+    plt.title("Active Devices per Week", fontsize=20, fontweight='bold')
+    plt.xlabel("Week", fontsize=16, fontweight='bold')
+    plt.ylabel("Hits", fontsize=16, fontweight='bold')
 
     plt.xlim([pd.to_datetime(START_DATE), pd.to_datetime(END_DATE)])
 
-    plt.xticks(rotation=45)
-    plt.legend()
+    plt.xticks(rotation=45, fontsize=14, fontweight='bold')
+    plt.yticks(fontsize=14, fontweight='bold')
+
+    plt.legend(fontsize=16, fontweight='bold')
     plt.tight_layout()
 
-    plt.savefig(f"growth_{fig}.png")
+    plt.savefig(f"growth_{fig}.png", dpi=80)
