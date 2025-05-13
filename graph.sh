56261e95703457f4bf89f05e2e846063764894a2
Niko <n.dunkel@gmail.com>
2025-05-13 13:24:44 -0700

    reset data fetching

--- graph.sh
+++ graph.sh
@@ -2,6 +2,6 @@
 
 pip3 install -r requirements.txt
 
-# wget https://data-analysis.fedoraproject.org/csv-reports/countme/totals.csv
+wget https://data-analysis.fedoraproject.org/csv-reports/countme/totals.csv
 
 python3 countme.py
