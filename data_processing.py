5ea8f868339f76ef949708a01a854540bd1e3dc1
gbrsni <72036069+gbrsni@users.noreply.github.com>
2026-01-21 15:16:46 +0100

    style: use double quotes for consistency

--- data_processing.py
+++ data_processing.py
@@ -68,7 +68,7 @@ def _load_and_process_data(
     fedora_repos_hits = (
         orig
         .filter(
-            pl.col('repo_tag').cast(pl.String).str.contains('^fedora-[0-9]+$')
+            pl.col("repo_tag").cast(pl.String).str.contains("^fedora-[0-9]+$")
         )
     )
 
